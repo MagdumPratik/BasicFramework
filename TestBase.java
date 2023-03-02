2d78bd8ab1216c6eea476028dd9918fd3efd4401
MagdumPratik <113919152+MagdumPratik@users.noreply.github.com>
2023-03-02 17:41:32 +0530

    Add files via upload

---
+++ TestBase.java
@@ -0,0 +1,46 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import helpers.RegisterHelper;
+import pageObject.RegisterPage;
+
+public class TestBase {
+	public static WebDriver driver;
+	
+	public static RegisterPage rp;
+	public static RegisterHelper rh;
+	
+	@BeforeClass
+	public void launchBrowser() {
+		System.out.println("Launching Browser.");
+        String baseDir = System.getProperty("user.dir");
+        System.setProperty("webdriver.chrome.driver",baseDir + "\\src\\test\\resources\\drivers\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        System.out.println("Browser Launched.");
+    }
+	 @BeforeClass(dependsOnMethods = "launchBrowser")
+	    public void initialize(){
+	        System.out.println("Initializing classes");
+	        rp = new RegisterPage(driver);
+	        rh = new RegisterHelper(driver);
+	        System.out.println("Initialization successful");
+	    }
+	 @BeforeClass(dependsOnMethods = "launchBrowser")
+	    public void navigateToBaseURL(){
+	        System.out.println("Navigating to Base URL");
+	        driver.get("https://demo.guru99.com/test/newtours/index.php");
+	        System.out.println("Navigation Successful");
+	    }
+	 @AfterClass
+	    public void closeDriver(){
+	        System.out.println("Closing Browser");
+	        driver.quit();
+	        System.out.println("Browser Closed");
+	    }
+	}
+	
